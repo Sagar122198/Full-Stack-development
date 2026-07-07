c32d7116978bd4feb18d44c88078df45d823e247
Sagar kumar <sagar8789297230kumar@gmail.com>
2026-07-07 12:38:55 +0530

    React

---
+++ JavaScript/Hero.js
@@ -0,0 +1,10 @@
+const Hero = ({ text }) => {
+    return (
+        <header className="bg-dark text-white p-5">
+            {text}
+            
+
+        </header>
+    )
+}
+export default Hero;
