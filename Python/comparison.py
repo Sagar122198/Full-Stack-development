6dc3aa882296558744105913cd52a6010136d42a
Sagar kumar <sagar8789297230kumar@gmail.com>
2026-07-10 12:05:29 +0530

    Python_101

---
+++ Python/comparison.py
@@ -0,0 +1,9 @@
+name = input("Enter your namae : ")
+if name== "sagar":
+    print(f"Hello {name}")
+elif name == "goat":
+    print(f"Hello {name}")
+elif name == "goater":
+    print(f"Hello {name}")
+else:
+    print("Enter the correct name")
