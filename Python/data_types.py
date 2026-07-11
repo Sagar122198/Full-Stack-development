9a1f054f4a0d6450908b7ec57f1390787ad17655
Sagar kumar <sagar8789297230kumar@gmail.com>
2026-07-11 11:44:41 +0530

    Python_101

---
+++ Python/data_types.py
@@ -0,0 +1,12 @@
+string = "this is a string"
+integer = 20
+float = 3.14
+boolean = True # or False.
+dictionary = {
+    "key" : "value"
+}
+list = [1,"2"]
+none = None 
+tuple = (1,float, boolean)
+sets = {1,3,"three"} # does not follow order.
+
