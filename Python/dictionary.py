61883b01e9c8c8dffac95c7b0ef880106aad5047
Sagar kumar <sagar8789297230kumar@gmail.com>
2026-07-11 11:45:15 +0530

    Python_101

---
+++ Python/dictionary.py
@@ -0,0 +1,9 @@
+dic = {
+    "key" : "value",
+    "lsjdk" : 12
+}
+print(dic)
+dic["new"] = "nnneeewww" # Add's this key , value.
+print(dic)
+del dic["key"]
+print(dic)
---
+++ Python/forLoop.py
@@ -0,0 +1,17 @@
+lst = [1 , "two" , 3 ]
+for i in lst:
+    print ( i )
+
+string = "Hello"
+
+for i in string:
+    print ( i )
+
+dictionary = {
+    "key" : "value",
+    "linkedIn" : "sagar-kumar-in",
+    "twitter" : 'sagarkumar_x'
+}
+
+for key,value in dictionary.items():
+    print (f"the key is {key} and value is  {value}")
