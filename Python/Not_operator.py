96857d0f996cc9a8c87259980833bd7ae5b272f8
Sagar kumar <sagar8789297230kumar@gmail.com>
2026-07-19 13:34:50 +0530

    Python_201

---
+++ Python/Not_operator.py
@@ -0,0 +1,9 @@
+boolean = False
+if not boolean :
+    print("boolean is true")
+else:
+    print("false")
+
+name = "Kane Ezki"
+if name not in ['Kalob', 'Jon', 'Gully']:
+    print("Kane is not part of the club")
